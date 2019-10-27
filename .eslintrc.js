69518ae9d5e11d339bc14f8e755b6d36a0d7083f
Andres <andresefr24@gmail.com>
2019-10-27 22:57:45 +0100

    microfix for precommit and prepush linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,27 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: ["plugin:prettier/recommended"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
     },
-    "extends": ["plugin:prettier/recommended"],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 2018,
-        "sourceType": "module",
-        "env":{
-            "browser": true,
-            "node": false,
-            "es6": true
-        }
-    },
-    "plugins": [
-        "react",
-        "prettier"
-    ],
-    "rules": {
-        "prettier/prettier": "error"
+    ecmaVersion: 2018,
+    sourceType: "module",
+    env: {
+      browser: true,
+      node: false,
+      es6: true
     }
+  },
+  plugins: ["react", "prettier"],
+  rules: {
+    "prettier/prettier": "error"
+  }
 };
